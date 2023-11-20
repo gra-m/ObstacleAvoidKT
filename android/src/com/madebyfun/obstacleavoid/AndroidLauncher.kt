46f958e51463e87e053932822d12cfe6fe30ed23
gra-m <grahamd483@gmail.com>
2023-11-20 17:00:48 +0100

    AndroidLauncher converted to Kotlin

--- android/src/com/madebyfun/obstacleavoid/AndroidLauncher.kt
+++ android/src/com/madebyfun/obstacleavoid/AndroidLauncher.kt
@@ -1,7 +1,13 @@
 //Created by Graham Duthie on 20/11/2023 16:53
 package com.madebyfun.obstacleavoid
-
+import android.os.Bundle
 import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
-class AndroidLauncher : AndroidApplication {
+class AndroidLauncher : AndroidApplication() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initialize(ObstacleAvoidKTGame(), AndroidApplicationConfiguration())
+    }
 }
