275a76396190f07f1bbe76bbf5aa72cd7634e1ea
gra-m <grahamd483@gmail.com>
2023-11-20 17:46:05 +0100

    Build was failing, put off upgrading Gradle and investigated the errors showing with Kotlin but upgrading gradle fixed. Now builds ok.

---
+++ core/src/com/madebyfun/obstacleavoid/screen/GameScreen.kt
@@ -0,0 +1,42 @@
+//Created by Graham Duthie on 20/11/2023 17:04
+package com.madebyfun.obstacleavoid.screen
+
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class GameScreen : Screen {
+
+    private lateinit var batch: SpriteBatch
+    private lateinit var img: Texture
+    
+    override fun show() {
+        batch = SpriteBatch()
+        img = Texture(FileHandle("badlogic.jpg"))
+    }
+
+    override fun render(delta: Float) {
+        //batch.use
+
+    }
+
+    override fun resize(width: Int, height: Int) {
+        //Viewport
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun hide() {
+        dispose()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
