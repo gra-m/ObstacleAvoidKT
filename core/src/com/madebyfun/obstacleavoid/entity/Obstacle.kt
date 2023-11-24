028d66d0a2b7906d9e87af22e6fb471371d4384c
gra-m <grahamd483@gmail.com>
2023-11-24 12:00:57 +0100

    As x,y initialization is clean there is no need for setter adjustment or explicit setters.

--- core/src/com/madebyfun/obstacleavoid/entity/Obstacle.kt
+++ core/src/com/madebyfun/obstacleavoid/entity/Obstacle.kt
@@ -7,35 +7,14 @@ import com.madebyfun.obstacleavoid.config.GameConfig
 import com.madebyfun.obstacleavoid.utils.circle
 
 // No need for init, setters set x and y from constructor parameters and bounds are initialized on declaration
-class Obstacle(x: Float, y: Float) {
+class Obstacle(private var x: Float, private var y: Float) {
     companion object {
         private const val BOUNDS_RADIUS = GameConfig.OBSTACLE_AND_PLAYER_RADIUS
         private const val SIZE = BOUNDS_RADIUS * 2
         private const val MAX_Y_SPEED = 0.15f
     }
-    private var x: Float = x
-        private set (value) {
-            val rightMostExtent = GameConfig.WORLD_WIDTH - BOUNDS_RADIUS
-            val leftMostExtent = BOUNDS_RADIUS
-            field = if (value > rightMostExtent)
-                rightMostExtent
-            else if (value < leftMostExtent)
-                leftMostExtent
-            else
-                value
-        }
 
-    private var y: Float = y
-        private set(value) {
-            val upperMostOutOfViewExtent = GameConfig.WORLD_HEIGHT + GameConfig.OBSTACLE_AND_PLAYER_RADIUS
-            val lowerMostOutOfViewExtent = 0 - SIZE
-           field = if (value > upperMostOutOfViewExtent)
-               upperMostOutOfViewExtent
-            else if (value < lowerMostOutOfViewExtent)
-                lowerMostOutOfViewExtent
-            else
-                value
-        }
+
     private var bounds = Circle(x, y, BOUNDS_RADIUS)
 
 
